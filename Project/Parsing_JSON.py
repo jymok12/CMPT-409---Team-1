d9c19bbd4b47bc45305771ed2473ec2bbff044f8
jymok12 <51149077+jymok12@users.noreply.github.com>
2022-07-11 01:36:55 -0700

    Updated

---
+++ Project/Parsing_JSON.py
@@ -0,0 +1,21 @@
+# %%
+import pandas as pd
+import os
+import json
+from datetime import datetime
+
+# %%
+df_data = pd.DataFrame()
+for file in os.listdir('G:\CMPT 409\CMPT-409---Team-1\Project'):
+    if file.endswith(".json"):
+        print(file)
+        df = pd.read_json(file)
+        data = pd.json_normalize(df['data'])
+        df_data = pd.concat([df_data, data])
+        df_data = df_data.dropna(how='all')
+df_data
+
+# %%
+df_data.to_csv('G:\CMPT 409\CMPT-409---Team-1\Project\combined_output.csv', index = False)
+
+
