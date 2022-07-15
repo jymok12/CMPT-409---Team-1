1dc6bf9349c4d4318a18998ff23925ec6a9cb607
jymok12 <51149077+jymok12@users.noreply.github.com>
2022-07-14 17:26:42 -0700

    Updated CSV

--- Project/Parsing_JSON.py
+++ Project/Parsing_JSON.py
@@ -5,10 +5,16 @@ import json
 from datetime import datetime
 
 # %%
-dict_from_csv = pd.read_csv('G:\CMPT 409\CMPT-409---Team-1\Project\high_diamond_ranked_10min.csv', header = None, index_col = 0, squeeze = True).to_dict()
+dict_from_csv = pd.read_csv('G:\CMPT 409\CMPT-409---Team-1\Project\high_diamond_ranked_10min.csv')
+old_value = dict_from_csv['gameId']
+minimum_value = dict_from_csv['gameId'].min()
+new_value = dict_from_csv['gameId'] - minimum_value
+dict_from_csv['gameId'].update(new_value)
+dict_from_csv.to_csv('G:\CMPT 409\CMPT-409---Team-1\Project\high_diamond_ranked_10min.csv(updated)')
 
+# %%
+dict_from_csv = pd.read_csv('G:\CMPT 409\CMPT-409---Team-1\Project\high_diamond_ranked_10min.csv(updated)', header = None, index_col = 0, squeeze = True).to_dict()
 with open('dict.json','w') as fp:
-    json.dump(dict_from_csv, fp)
-dict_from_csv
+     json.dump(dict_from_csv, fp)
 
 
